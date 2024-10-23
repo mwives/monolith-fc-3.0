69624363fa5b56eb17836ee775abb8915b82c705
Ives M. W <ivesmw@gmail.com>
2024-10-23 14:07:38 -0300

    feat: add ProductRepository and implement findAll

---
+++ src/modules/store-catalog/repository/product.repository.spec.ts
@@ -0,0 +1,52 @@
+import { Sequelize } from 'sequelize-typescript'
+
+import { ProductModel } from '@store-catalog/repository/product.model'
+import { ProductRepository } from '@store-catalog/repository/product.repository'
+import { Id } from '@shared/domain/value-object/id.value-object'
+
+describe('ProductRepository', () => {
+  let sequelize: Sequelize
+
+  const productProps = {
+    name: 'any_name',
+    description: 'any_description',
+    salesPrice: 100,
+  }
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: 'sqlite',
+      storage: ':memory:',
+      logging: false,
+      sync: { force: true },
+    })
+
+    sequelize.addModels([ProductModel])
+    await sequelize.sync()
+  })
+
+  afterEach(async () => {
+    await sequelize.close()
+  })
+
+  describe('findAll', () => {
+    it('should return all products', async () => {
+      await ProductModel.create({ ...productProps, id: new Id().value })
+      await ProductModel.create({ ...productProps, id: new Id().value })
+
+      const productRepository = new ProductRepository()
+
+      const products = await productRepository.findAll()
+
+      expect(products).toHaveLength(2)
+      expect(products[0]).toMatchObject({
+        id: expect.any(Id),
+        ...productProps,
+      })
+      expect(products[1]).toMatchObject({
+        id: expect.any(Id),
+        ...productProps,
+      })
+    })
+  })
+})
---
+++ src/modules/store-catalog/repository/product.repository.ts
@@ -0,0 +1,22 @@
+import { Id } from '@shared/domain/value-object/id.value-object'
+import { Product } from '@store-catalog/domain/product.entity'
+import { ProductGateway } from '@store-catalog/gateway/product.gateway'
+import { ProductModel } from '@store-catalog/repository/product.model'
+
+export class ProductRepository implements ProductGateway {
+  async findAll(): Promise<Product[]> {
+    const products = await ProductModel.findAll()
+    return products.map(({ dataValues: product }) => {
+      return new Product({
+        id: new Id(product.id),
+        name: product.name,
+        description: product.description,
+        salesPrice: product.salesPrice,
+      })
+    })
+  }
+
+  async findById(id: string): Promise<Product> {
+    throw new Error('Method not implemented.')
+  }
+}
