f69d4af4db1e6ca73d7c18795a47e23d7a5555c9
Ives M. W <ivesmw@gmail.com>
2024-10-25 18:09:52 -0300

    feat: implement create invoice

---
+++ src/modules/invoice/repository/invoice.repository.spec.ts
@@ -0,0 +1,70 @@
+import { Sequelize } from 'sequelize-typescript'
+
+import { Invoice } from '@invoice/domain/entity/invoice'
+import { InvoiceItem } from '@invoice/domain/entity/invoice-item'
+import { Address } from '@invoice/domain/value-object/address'
+import { Id } from '@shared/domain/value-object/id.value-object'
+import { InvoiceItemModel } from './invoice-item.model'
+import { InvoiceModel } from './invoice.model'
+import { InvoiceRepository } from './invoice.repository'
+
+describe('InvoiceRepository', () => {
+  let sequelize: Sequelize
+  const invoiceRepository = new InvoiceRepository()
+
+  const invoice = new Invoice({
+    id: new Id('any_id'),
+    name: 'any_name',
+    document: 'any_document',
+    address: new Address({
+      city: 'any_city',
+      complement: 'any_complement',
+      number: 'any_number',
+      state: 'any_state',
+      street: 'any_street',
+      zipCode: 'any_zipCode',
+    }),
+    items: [
+      new InvoiceItem({
+        invoiceId: new Id('any_id'),
+        name: 'any_name',
+        price: 100,
+      }),
+    ],
+  })
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: 'sqlite',
+      storage: ':memory:',
+      logging: false,
+      sync: { force: true },
+    })
+
+    sequelize.addModels([InvoiceModel, InvoiceItemModel])
+    await sequelize.sync()
+  })
+
+  afterEach(async () => {
+    await sequelize.close()
+  })
+
+  describe('findById', () => {
+    it('should find an invoice by id', async () => {
+      expect(true).toBe(true)
+    })
+  })
+
+  describe('create', () => {
+    it('should create an invoice', async () => {
+      const createdInvoice = await invoiceRepository.create(invoice)
+
+      expect(createdInvoice.id.value).toBe(invoice.id.value)
+      expect(createdInvoice).toMatchObject({
+        ...invoice,
+        _createdAt: expect.any(Date),
+        _updatedAt: expect.any(Date),
+      })
+    })
+  })
+})
---
+++ src/modules/invoice/repository/invoice.repository.ts
@@ -0,0 +1,74 @@
+import { Invoice } from '@invoice/domain/entity/invoice'
+import { InvoiceGateway } from '@invoice/gateway/invoice.gateway'
+import { InvoiceModel } from './invoice.model'
+import { InvoiceItemModel } from './invoice-item.model'
+import { Address } from '@invoice/domain/value-object/address'
+import { Id } from '@shared/domain/value-object/id.value-object'
+import { InvoiceItem } from '@invoice/domain/entity/invoice-item'
+
+export class InvoiceRepository implements InvoiceGateway {
+  async findById(id: string): Promise<Invoice> {
+    throw new Error('Method not implemented.')
+  }
+
+  async create(invoice: Invoice): Promise<Invoice> {
+    const transaction = await InvoiceModel.sequelize.transaction()
+
+    try {
+      const invoiceData = {
+        id: invoice.id.value,
+        name: invoice.name,
+        document: invoice.document,
+        street: invoice.address.street,
+        number: invoice.address.number,
+        complement: invoice.address.complement,
+        city: invoice.address.city,
+        state: invoice.address.state,
+        zipCode: invoice.address.zipCode,
+      }
+
+      const createdInvoice = await InvoiceModel.create(invoiceData, {
+        transaction,
+      })
+
+      const invoiceItems = invoice.items.map((item) => ({
+        id: item.id.value,
+        invoiceId: invoice.id.value,
+        name: item.name,
+        price: item.price,
+      }))
+
+      await InvoiceItemModel.bulkCreate(invoiceItems, { transaction })
+
+      await transaction.commit()
+
+      return new Invoice({
+        id: new Id(createdInvoice.id),
+        name: createdInvoice.name,
+        document: createdInvoice.document,
+        address: new Address({
+          street: createdInvoice.street,
+          number: createdInvoice.number,
+          complement: createdInvoice.complement,
+          city: createdInvoice.city,
+          state: createdInvoice.state,
+          zipCode: createdInvoice.zipCode,
+        }),
+        items: invoiceItems.map(
+          (item) =>
+            new InvoiceItem({
+              id: new Id(item.id),
+              invoiceId: new Id(item.invoiceId),
+              name: item.name,
+              price: item.price,
+            })
+        ),
+        createdAt: createdInvoice.createdAt,
+        updatedAt: createdInvoice.updatedAt,
+      })
+    } catch (error) {
+      await transaction.rollback()
+      throw error
+    }
+  }
+}
