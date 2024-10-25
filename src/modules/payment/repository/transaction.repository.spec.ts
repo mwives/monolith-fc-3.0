11ffdf03698399e33507fe26a4cb6a5daad9fcb1
Ives M. W <ivesmw@gmail.com>
2024-10-25 16:47:32 -0300

    feat: add Transaction model and repository

---
+++ src/modules/payment/repository/transaction.repository.spec.ts
@@ -0,0 +1,61 @@
+import { Sequelize } from 'sequelize-typescript'
+
+import { Id } from '@shared/domain/value-object/id.value-object'
+import { TransactionModel } from './transaction.model'
+import { Transaction } from '@payment/domain/transaction'
+import { TransactionRepository } from './transaction.repository'
+
+describe('TransactionRepository', () => {
+  let sequelize: Sequelize
+
+  const clientProps = {
+    id: new Id('1'),
+    name: 'any_name',
+    email: 'any_email',
+    address: 'any_address',
+  }
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: 'sqlite',
+      storage: ':memory:',
+      logging: false,
+      sync: { force: true },
+    })
+
+    sequelize.addModels([TransactionModel])
+    await sequelize.sync()
+  })
+
+  afterEach(async () => {
+    await sequelize.close()
+  })
+
+  describe('save', () => {
+    it('should create a transaction', async () => {
+      const transaction = new Transaction({
+        id: new Id('1'),
+        orderId: 'any_order_id',
+        amount: 10,
+        status: 'approved',
+      })
+
+      const transactionRepository = new TransactionRepository()
+
+      await transactionRepository.save(transaction)
+
+      const transactionCreated = await (
+        await TransactionModel.findByPk(transaction.id.value)
+      ).dataValues
+
+      expect(transactionCreated).toMatchObject({
+        id: transaction.id.value,
+        orderId: transaction.orderId,
+        amount: transaction.amount,
+        status: transaction.status,
+        createdAt: expect.any(Date),
+        updatedAt: expect.any(Date),
+      })
+    })
+  })
+})
---
+++ src/modules/payment/repository/transaction.repository.ts
@@ -0,0 +1,26 @@
+import { Transaction } from '@payment/domain/transaction'
+import { TransactionGateway } from '@payment/gateway/transaction.gateway'
+import { TransactionModel } from './transaction.model'
+import { Id } from '@shared/domain/value-object/id.value-object'
+
+export class TransactionRepository implements TransactionGateway {
+  async save(input: Transaction): Promise<Transaction> {
+    await TransactionModel.create({
+      id: input.id.value,
+      orderId: input.orderId,
+      amount: input.amount,
+      status: input.status,
+      createdAt: input.createdAt,
+      updatedAt: input.updatedAt,
+    })
+
+    return new Transaction({
+      id: input.id,
+      orderId: input.orderId,
+      amount: input.amount,
+      status: input.status,
+      createdAt: input.createdAt,
+      updatedAt: input.updatedAt,
+    })
+  }
+}
